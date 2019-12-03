683cd8224fe5c64ef1d25849200087fda02fb2d3
kowaraj <kowaraj4stuff@gmail.com>
2019-12-03 18:56:48 +0100

    Simple React-App template

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>my First React App</h1>
+      <p>Welcome</p>
+    </div>
+  );
+}
+
+export default App;
